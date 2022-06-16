a1dfecb88b3cf1915072db3e4881c2c420163b09
kondelow <achmadrasidi@gmail.com>
2022-06-16 15:41:18 +0700

    add cloudinary admin

--- src/Components/Product/ShopDetailAdmin.js
+++ src/Components/Product/ShopDetailAdmin.js
@@ -208,7 +208,7 @@ const ShopDetailAdmin = () => {
                         <div className="row text-center">
                           <div className="col-md-6">
                             {" "}
-                            <img src={`${process.env.REACT_APP_API}${product.image}`} className="bottom-image" alt="" />
+                            <img src={product.image} className="bottom-image" alt="" />
                           </div>
                           <div className="col-md-6">
                             {" "}
